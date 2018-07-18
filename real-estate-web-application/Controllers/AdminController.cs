52f9a595062bc95a82cb3e78ef204f80a5d30ad3
Fatih <fatih47154@gmail.com>
2018-07-18 22:16:38 +0300

    _AdminLayout düzenlendi, siteOzellikleri.cshtml eklendi.

--- real-estate-web-application/Controllers/AdminController.cs
+++ real-estate-web-application/Controllers/AdminController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using real_estate_web_application.Models;
 
 namespace real_estate_web_application.Controllers
 {
     public class AdminController : Controller
     {
+        emlakDB db = new emlakDB();
         // GET: Admin
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult siteOzellikleri()
+        {
+            var goster = db.SiteOzellikleri.ToList();
+            var a = goster.FirstOrDefault(x => x.siteID == 1);
+            ViewBag.siteOzellikleri = a;
             return View();
         }
     }
