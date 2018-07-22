bef5943b85d65a0c4f79aee03004105b9fe5bc0f
emre <me@kaymakemre.com>
2018-07-22 14:58:59 +0300

    Emlak Ana sayfa giriş

---
+++ real-estate-web-application/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using real_estate_web_application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace real_estate_web_application.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        emlakDB db = new emlakDB();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hakkimizda()
+        {
+
+            return View();
+        }
+    }
+}
