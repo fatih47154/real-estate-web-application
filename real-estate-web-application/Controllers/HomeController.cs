60b536b9325756b16b35bf1cd46558cba314cdf0
mehmetozcelik <mehmetozcelik.4181@gmail.com>
2018-07-25 21:05:00 +0300

    HOme İndex'te Sayma işlemleri eklendi

--- real-estate-web-application/Controllers/HomeController.cs
+++ real-estate-web-application/Controllers/HomeController.cs
@@ -13,8 +13,15 @@ namespace real_estate_web_application.Controllers
         emlakDB db = new emlakDB();
         public ActionResult Index()
         {
-           var ilanlar = db.Ilan.ToList();
-           ViewBag.iln = ilanlar;           
+            var kln = db.Kullanicilar.ToList();            
+            var ilanlar = db.Ilan.ToList();
+            var yorum = db.yorum.ToList();
+            ViewBag.kullaniciSayisi = kln.LongCount();
+            ViewBag.ilanSayisi = ilanlar.LongCount();
+            ViewBag.yorumSayisi = yorum.LongCount();           
+            ViewBag.iln = ilanlar;
+            
+                      
                                  
             return View();
         }
